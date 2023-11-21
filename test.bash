4f2da9f20bcf4c7286083a5d125161ddce660450
Nozaki <sorakara656@gmail.com>
2023-11-21 14:17:16 +0900

    testの改修2

--- test.bash
+++ test.bash
@@ -1,15 +1,29 @@
 #!/bin/bash
-# SPDX-FileCopyrightText: 2023 Nozaki
-# SPDX-License-Identifier: BSD-3-Clause　
+
+#SPDX-FileCopyrightText: 2023 Nozaki
+#SPDX-License-Identifier: BSD-3-Clause
 ng () {
-      echo NG at Line $1
-      res=1
+    echo "NG at Line $1"
+    res=1
 }
+
 res=0
 
-out=$(seq 2 | ./plus)
-_z="素数じゃない"
-[ "${out}" = 3 ] || [ "${z}" !=  "${expected_z}" ] || ng ${LINENO}i
-[ "$res" = 0 ] && echo OK 
-exit $[res]
+output=$(./plus)
 
+#1行目の出力が3であるか確認
+if [ "${output[0]}" == "3" ]; then
+    echo "First output is 3: ${output[0]}"
+else
+    ng $LINENO
+fi
+
+#2行目の出力が素数ゥであるか確認
+if [ "${output[1]}" == "素数ゥ" ]; then
+    echo "Second output indicates a prime number: ${output[1]}"
+else
+    ng $LINENO
+fi
+
+[ "$res" = 0 ] && echo "OK"
+exit $res
